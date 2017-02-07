0339b5239c927863445ad34f8a1fd62268c157bb
Rudi Persson <rudi0234@edu.easj.dk>
2017-02-07 10:25:27 +0100

    Opage 1 er løst. Trot jeg :) .
    HAr tilføjet regnestykked med afgifter

---
+++ car Inheritance/Bil.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_Inheritance
+{
+     public class Bil
+    {
+        public int BilPrisExAfgift { get; private set; }
+        public int KøbsÅr { get; private set; }
+        public string Mærke { get; private set; }
+        public string RegistreringsNr { get; set; }
+        public int KmPrLiter { get; private set; }
+
+
+        /*Methoder*/
+        public virtual int RegistreringsAfgift()
+        {
+            int BilAfgift = 0;
+
+            if (KøbsÅr <= 2014)
+            {
+                if (BilPrisExAfgift <= 80500)
+                {
+                  BilAfgift =  (BilPrisExAfgift * 105) / 100;
+                }
+
+                if (BilPrisExAfgift > 80500)
+                {
+                    int sum1 = (80500 * 105) / 100;
+                    int sum2 = ((BilPrisExAfgift - 80500) * 180) / 100;
+                    BilAfgift = sum1 + sum2;
+                }
+
+                
+            }
+                else if (KøbsÅr >= 2015)
+            {
+                if (BilPrisExAfgift <= 81700)
+                {
+                    BilAfgift = (BilPrisExAfgift * 105) / 100;
+                }
+
+                if (BilPrisExAfgift > 81700)
+                {
+                    int sum1 = (80500 * 105) / 100;
+                    int sum2 = ((BilPrisExAfgift - 80500) * 180) / 100;
+                    BilAfgift = sum1 + sum2;
+                }
+
+            }
+
+            return BilAfgift;
+        }
+
+        public int TotalPris()
+        {
+            return BilPrisExAfgift + RegistreringsAfgift();
+        } 
+
+        public virtual int HalvÅrligEjerafgift()
+        {
+            return 1000;
+        }
+
+
+        public Bil(string mærke, int prisExAfgift, int købsÅr, int KmPrLiter)
+        {
+            this.Mærke = mærke;
+            this.BilPrisExAfgift = prisExAfgift;
+            this.KøbsÅr = købsÅr;
+            this.KmPrLiter = KmPrLiter;
+        }
+
+    }
+}
--- car Inheritance/Program.cs
+++ car Inheritance/Program.cs
@@ -10,6 +10,10 @@ namespace car_Inheritance
     {
         static void Main(string[] args)
         {
+            Bil b1 = new Bil("ford", 81701, 2015, 200);
+
+
+            Console.WriteLine(b1.TotalPris()); 
         }
     }
 }
