efa5a7c477001fcc14e71057959934f61c472908
Rudi Persson <rudi0234@edu.easj.dk>
2017-02-08 10:01:21 +0100

    Opgave 5 løst. Nu er der også en Benzin bil klasse, som også udregner ejerafgift.

---
+++ car Inheritance/BenzinBil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_Inheritance
+{
+    sealed class BenzinBil : Bil
+    {
+
+        /*Constructor*/
+        public BenzinBil(string mærke, int prisExAfgift, int KøbsÅr, string registreringsNr, int KmPrLiter) 
+            : base(mærke, prisExAfgift, KøbsÅr, registreringsNr, KmPrLiter)
+        {
+           
+        }
+
+
+        /*Methods*/
+        public override int HalvÅrligEjerafgift()
+        {
+            int udgift = 0;
+
+            if (KmPrLiter < 20)
+            {
+                udgift = 1200;
+            }
+            else if (KmPrLiter >= 20 && KmPrLiter <= 28)
+            {
+                udgift = 600;
+            }
+            else if (KmPrLiter > 28)
+            {
+                udgift = 320;
+            }
+
+            return udgift;
+        }
+    }
+}
--- car Inheritance/Program.cs
+++ car Inheritance/Program.cs
@@ -10,21 +10,19 @@ namespace car_Inheritance
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("------------------------Diesel Biler---------------------------");
+            /*Diesel Bil*/
             List<DieselBil> dieselBilListe = new List<DieselBil>();
             DieselBil d1 = new DieselBil("ford", 100000, 2014, "dad12", 11);
             DieselBil d2 = new DieselBil("Toyota", 100000, 2014, "dad13", 26);
             DieselBil d3 = new DieselBil("Ferrari", 100000, 2014, "dad14", 11);
             DieselBil d4 = new DieselBil("Volvo", 100000, 2014, "dad15", 11, false);
             DieselBil d5 = new DieselBil("Smart Car", 100000, 2014, "dad16", 11, false);
-
-
             dieselBilListe.Add(d1);
             dieselBilListe.Add(d2);
             dieselBilListe.Add(d3);
             dieselBilListe.Add(d4);
             dieselBilListe.Add(d5);
-
 
             foreach (var DieselCar in dieselBilListe)
             {
@@ -32,7 +30,28 @@ namespace car_Inheritance
                 Console.WriteLine(DieselCar.HalvÅrligEjerafgift());
             }
 
-       
+
+            Console.WriteLine("------------------------Benzin Biler---------------------------");
+            /*Benzin Bil*/
+            List<BenzinBil> benzinBilListe = new List<BenzinBil>();
+            BenzinBil b1 = new BenzinBil("ford", 100000, 2014, "dad13", 11);
+            BenzinBil b2 = new BenzinBil("Toyota", 100000, 2014, "dad14", 20);
+            BenzinBil b3 = new BenzinBil("Ferrari", 100000, 2014, "dad5532", 28);
+            BenzinBil b4 = new BenzinBil("Volvo", 100000, 2014, "dad1243", 11);
+            BenzinBil b5 = new BenzinBil("Smart Car", 100000, 2014, "dad1412", 29);
+
+            benzinBilListe.Add(b1);
+            benzinBilListe.Add(b2);
+            benzinBilListe.Add(b3);
+            benzinBilListe.Add(b4);
+            benzinBilListe.Add(b5);
+
+            foreach (var BenzinBIl in benzinBilListe)
+            {
+                Console.WriteLine(BenzinBIl.RegistreringsNr);
+                Console.WriteLine(BenzinBIl.HalvÅrligEjerafgift());
+            }
+
         }
     }
 }
